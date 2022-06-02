c28ac67c4f27e951bc05c4357332f894940b2821
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2022-06-01 22:44:17 -0300

    first commit

---
+++ portifolio/src/componentes/conhecimentos/index.js
@@ -0,0 +1,28 @@
+import './conhecimentos.scss'
+
+
+function Conhecimentos(){
+   return (
+    <section className="conhecimentos">
+        <h1 className="titulo destaquePrimeiraLetra">Linguagens e ferramentas</h1> 
+        <div className="conhecimentosImgBloco"> 
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" alt="logo"/>   
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg"alt="logo"/>       
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="logo" />    
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="logo" />    
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" alt="logo"  />      
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-plain-wordmark.svg" alt="logo" />        
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg" alt="logo"  /> 
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" alt="logo"  />
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="logo"/>   
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eslint/eslint-original-wordmark.svg" alt="logo"  /> 
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" alt="logo"  /> 
+            <img className="conhecimentoImg"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="logo" /> 
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" alt="logo" />
+            <img className="conhecimentoImg" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="logo"  />  
+        </div> 
+     </section>        
+   )
+}
+
+export default Conhecimentos
---
+++ portifolio/src/componentes/projetos/index.js
@@ -0,0 +1,59 @@
+import {Card, Button } from 'react-bootstrap';
+import './projetos.scss'
+import brafood from 'assets/projetos/brafood.png'
+import eventsCalendar from 'assets/projetos/eventsCalendar.png'
+import aBenditaItalia from 'assets/projetos/a-bendita-italia.png'
+import typerGame from 'assets/projetos/typerGame.png'
+
+
+const projetos = [
+    {titulo: 'BraFood',
+     descricao: 'Um e-commerce de vegetais orgânicos com React, styled-compoments, context-api e material-ui ',
+    img: brafood,
+    link:'https://brafoods.vercel.app/',
+    linkGitHub:'https://github.com/brunom764/braFood'},
+    {titulo: 'Events calendar',
+     descricao: 'Um calendário atualizado com eventos com uso de React e a biblioeteca recoil',
+    img: eventsCalendar,
+    link:'https://events-calendar-lovat.vercel.app/',
+    linkGitHub:'https://github.com/brunom764/events-calendar'},
+    {titulo: 'A Bendita Italia',
+     descricao: 'Site de um restaurante italiano com React, TypeScript, Saas e Bootstrap.',
+    img: aBenditaItalia,
+    link:'https://a-bendita-italia.vercel.app/',
+    linkGitHub:''},
+    {titulo: 'TyperGame',
+     descricao: 'Um jogo de digitação de frases rapidas com o uso do jQuery',
+    img: typerGame,
+    link:' https://typer-game.vercel.app/',
+    linkGitHub:'https://github.com/brunom764/TyperGame'},
+]
+
+function Projetos (){
+   return (
+    <div className='projetos'>
+        <h1 className='titulo destaquePrimeiraLetra'>Meus projetos</h1>
+        <section className='cardsProjetos'>
+        {projetos.map(projeto => {
+            return (
+                <Card className='card'>
+                <Card.Img variant="top" src={projeto.img} className='cardImg' />
+                <Card.Body>
+                <Card.Title className='textoDestaque'>{projeto.titulo}</Card.Title>
+                <Card.Text>
+                    {projeto.descricao}
+                </Card.Text>
+                </Card.Body>
+                <Card.Body className='Buttonlinks'>
+                <Card.Link href={projeto.link}><Button variant="dark" className='textoDestaque'>Acessar o site</Button></Card.Link>
+                <Card.Link href={projeto.linkGitHub}><Button variant="secondary">Acessar código</Button></Card.Link>
+                </Card.Body>
+            </Card>
+            )
+            })};
+        </section>
+    </div>
+        )
+}
+
+export default Projetos;
